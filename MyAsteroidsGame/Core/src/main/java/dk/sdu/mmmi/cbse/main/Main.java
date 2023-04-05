f1050623793fe1b36da1785155d54cdd1ea92794
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-05 07:44:22 +0200

    no change

--- MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -4,9 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		LwjglApplicationConfiguration cfg =
 			new LwjglApplicationConfiguration();
 		cfg.title = "Asteroids";
@@ -14,9 +14,9 @@ public class Main {
 		cfg.height = 400;
 		cfg.useGL30 = false;
 		cfg.resizable = false;
-		
+
 		new LwjglApplication(new Game(), cfg);
-		
+
 	}
-	
+
 }
