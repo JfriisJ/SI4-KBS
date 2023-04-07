2d7c9240b50ef4098752799be76162f1e1669905
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-07 22:51:43 +0200

    working on bullet

--- MyAsteroidsGame/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerControlSystem.java
+++ MyAsteroidsGame/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerControlSystem.java
@@ -1,14 +1,14 @@
 package dk.sdu.mmmi.cbse.playersystem;
 
+import dk.sdu.mmmi.cbse.bulletsystem.BulletPlugin;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
-import static dk.sdu.mmmi.cbse.common.data.GameKeys.LEFT;
-import static dk.sdu.mmmi.cbse.common.data.GameKeys.RIGHT;
-import static dk.sdu.mmmi.cbse.common.data.GameKeys.UP;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.*;
 import static java.lang.Math.sqrt;
 
 /**
@@ -27,6 +27,12 @@ public class PlayerControlSystem implements IEntityProcessingService {
             movingPart.setLeft(gameData.getKeys().isDown(LEFT));
             movingPart.setRight(gameData.getKeys().isDown(RIGHT));
             movingPart.setUp(gameData.getKeys().isDown(UP));
+
+            if (gameData.getKeys().isDown(SPACE)){
+                BulletPlugin bullet = new BulletPlugin();
+                bullet.createBullet(gameData, player);
+                System.out.println("Space is pressed");
+            }
             
             
             movingPart.process(gameData, player);
