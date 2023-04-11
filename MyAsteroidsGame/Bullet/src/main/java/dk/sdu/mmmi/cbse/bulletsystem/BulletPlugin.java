43e4c98cd2185c646c5cf8a72c6306965f7b85bf
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-11 08:40:58 +0200

    created the bullet class and inserted javadoc

--- MyAsteroidsGame/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletPlugin.java
+++ MyAsteroidsGame/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletPlugin.java
@@ -1,41 +1,87 @@
 package dk.sdu.mmmi.cbse.bulletsystem;
 
+import com.badlogic.gdx.math.MathUtils;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 public class BulletPlugin implements IGamePluginService {
 
-    Bullet bullet;
-    @Override
-    public void start(GameData gameData, World world) {
-        this.bullet = new Bullet();
-        bullet.add(new MovingPart(10, 200, 300, 5));
-        world.addEntity(new Bullet());
+    /**
+     * The bullet entity created by the plugin.
+     */
+    Entity bullet;
 
+    /**
+     * The shooter entity used to create the bullet.
+     */
+    Entity shooter;
 
+    public BulletPlugin(Entity shooter) {
+        this.shooter = shooter;
     }
 
-    public Bullet createBullet(GameData gameData, Entity shooter) {
-        PositionPart positionPart = shooter.getPart(PositionPart.class);
-        float deacceleration = 10;
-        float acceleration = 200;
-        float maxSpeed = 300;
-        float rotationSpeed = 5;
-        float x = positionPart.getX();
-        float y = positionPart.getY();
-        float radians = 3.1415f / 2;
+    /**
+     * Starts the plugin by adding the bullet entity to the game world.
+     *
+     * @param GameData gameData The game data object.
+     * @param World world    The game world object.
+     */
+    @Override
+    public void start(GameData gameData, World world) {
+        // Add entities to the world
+        bullet = createBullet(gameData);
+        world.addEntity(bullet);
+    }
 
-        bullet = new Bullet();
-        bullet.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+    /**
+     * Creates a new bullet entity based on the shooter entity's position.
+     *
+     * @param GameData gameData The game data object.
+     * @return The new bullet entity.
+     */
+    public Entity createBullet(GameData gameData) {
+        PositionPart shooterPositionPart = this.shooter.getPart(PositionPart.class);
+        MovingPart shooterMovingPart = this.shooter.getPart(MovingPart.class);
+
+        float deacceleration = 0;
+        float acceleration = 0;
+        float maxSpeed = 350;
+        float rotationSpeed = 0;
+        float radians = shooterPositionPart.getRadians();
+
+        Entity bullet = new Bullet();
+
+        bullet.setRadius(1);
+
+        float bx = MathUtils.cos(radians) * this.shooter.getRadius() * bullet.getRadius();
+        float by = MathUtils.sin(radians) * this.shooter.getRadius() * bullet.getRadius();
+
+        float x = bx + shooterPositionPart.getX();
+        float y = by + shooterPositionPart.getY();
+
+        float shootSpeed = 350 + shooterMovingPart.getSpeed();
+
+        bullet.setShapeX(new float[4]);
+        bullet.setShapeY(new float[4]);
+        bullet.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed, shootSpeed));
         bullet.add(new PositionPart(x, y, radians));
+        bullet.add(new LifePart(1, 1));
+
 
         return bullet;
     }
 
+    /**
+     * Stops the plugin by removing the bullet entity from the game world.
+     *
+     * @param GameData gameData The game data object.
+     * @param World world The game world object.
+     */
     @Override
     public void stop(GameData gameData, World world) {
         world.removeEntity(this.bullet);
