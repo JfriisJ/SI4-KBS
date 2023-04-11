98f77fc420ef601d97a19c5234ebf9e83be28200
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-11 15:18:27 +0200

    removed final Gamedata

--- MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
+++ MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -32,7 +32,7 @@ public class Game implements ApplicationListener {
 
     private static OrthographicCamera cam;
     private ShapeRenderer sr;
-    private final GameData gameData = new GameData();
+    private GameData gameData = new GameData();
     private List<IEntityProcessingService> entityProcessors = new ArrayList<>();
     private List<IGamePluginService> entityPlugins = new ArrayList<>();
     private List<IPostEntityProcessingService> postEntityProcessors = new ArrayList<>();
