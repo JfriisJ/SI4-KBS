6d82cb38a8bc4fc6b40484a2c83a672b1a1cbfeb
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-12 11:22:44 +0200

    removed unused variables and methods

--- MyAsteroidsGame/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/Asteroid.java
+++ MyAsteroidsGame/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/Asteroid.java
@@ -4,14 +4,4 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 
 public class Asteroid extends Entity {
 
-//    private AsteroidType type;
-//
-//    public Asteroid(AsteroidType type)
-//    {
-//        this.type = type;
-//    }
-//
-//    public String getSize() {
-//        return type.getSize();
-//    }
 }
