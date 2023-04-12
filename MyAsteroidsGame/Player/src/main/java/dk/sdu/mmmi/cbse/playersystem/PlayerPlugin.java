f4a0afedf3ae72f85e17f49f91cd2adadefff276
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-12 14:19:06 +0200

    changed the timer on player ship shoot component

--- MyAsteroidsGame/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerPlugin.java
+++ MyAsteroidsGame/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerPlugin.java
@@ -38,7 +38,7 @@ public class PlayerPlugin implements IGamePluginService {
         playerShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
         playerShip.add(new PositionPart(x, y, radians));
         playerShip.add(new LifePart(3,0));
-        playerShip.add(new ShootingPart(0.001f));
+        playerShip.add(new ShootingPart(0.2f));
         
         return playerShip;
     }
