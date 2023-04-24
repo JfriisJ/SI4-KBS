725e97b4278f712b7d41217c7cd7fb01cfc7b47b
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-24 13:21:33 +0200

    changed the bullet module

--- MyAsteroidsGame/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
+++ MyAsteroidsGame/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
@@ -1,33 +1,44 @@
 package dk.sdu.mmmi.cbse.bulletsystem;
 
 import com.badlogic.gdx.math.MathUtils;
+import dk.sdu.mmmi.cbse.BulletSPI;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.TimerPart;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 
-public class BulletControlSystem implements IEntityProcessingService {
+public class BulletControlSystem implements IEntityProcessingService, BulletSPI {
     @Override
     public void process(GameData gameData, World world) {
 
         for (Entity bullet : world.getEntities(Bullet.class)) {
             PositionPart positionPart = bullet.getPart(PositionPart.class);
             MovingPart movingPart = bullet.getPart(MovingPart.class);
-            LifePart lifePart = bullet.getPart(LifePart.class);
-
-            movingPart.process(gameData, bullet);
-            positionPart.process(gameData, bullet);
-            lifePart.reduceExpiration(gameData.getDelta());
-            lifePart.process(gameData, bullet);
+            TimerPart timerPart = bullet.getPart(TimerPart.class);
 
             movingPart.setUp(true);
-
-            if (lifePart.getExpiration() < 0 || lifePart.isIsHit()){
+            if (timerPart.getExpiration() < 0) {
                 world.removeEntity(bullet);
             }
+
+            timerPart.process(gameData, bullet);
+            movingPart.process(gameData, bullet);
+            positionPart.process(gameData, bullet);
+
+//            LifePart lifePart = bullet.getPart(LifePart.class);
+
+//            lifePart.reduceExpiration(gameData.getDelta());
+//            lifePart.process(gameData, bullet);
+
+
+
+//            if (lifePart.getExpiration() < 0 || lifePart.isIsHit()){
+//                world.removeEntity(bullet);
+//            }
             updateShape(bullet);
         }
 
@@ -42,21 +53,55 @@ public class BulletControlSystem implements IEntityProcessingService {
         float y = positionPart.getY();
         float radians = positionPart.getRadians();
 
-        shapex[0] = x + 2 * MathUtils.cos(radians + 3.1415f / 2);
-        shapey[0] = y + 2 * MathUtils.sin(radians + 3.1415f / 2);
+        shapex[0] = x;
+        shapey[0] = y;
 
-        shapex[1] = x + 2 * MathUtils.cos(radians + 3.1415f + 3.1415f / 4);
-        shapey[1] = y + 2 * MathUtils.sin(radians + 3.1415f + 3.1415f / 4);
+        shapex[1] = x + MathUtils.cos(radians - 4 * 3.1415f / 5);
+        shapey[1] = y + MathUtils.sin(radians - 4 * 3.1415f / 5);
 
-        shapex[2] = x + 2 * MathUtils.cos(radians + 3.1415f);
-        shapey[2] = y + 2 * MathUtils.sin(radians + 3.1415f);
-
-        shapex[3] = x + 2 * MathUtils.cos(radians + 3.1415f - 3.1415f / 4);
-        shapey[3] = y + 2 * MathUtils.sin(radians + 3.1415f - 3.1415f / 4);
+//        shapex[0] = x + 2 * MathUtils.cos(radians + 3.1415f / 2);
+//        shapey[0] = y + 2 * MathUtils.sin(radians + 3.1415f / 2);
+//
+//        shapex[1] = x + 2 * MathUtils.cos(radians + 3.1415f + 3.1415f / 4);
+//        shapey[1] = y + 2 * MathUtils.sin(radians + 3.1415f + 3.1415f / 4);
+//
+//        shapex[2] = x + 2 * MathUtils.cos(radians + 3.1415f);
+//        shapey[2] = y + 2 * MathUtils.sin(radians + 3.1415f);
+//
+//        shapex[3] = x + 2 * MathUtils.cos(radians + 3.1415f - 3.1415f / 4);
+//        shapey[3] = y + 2 * MathUtils.sin(radians + 3.1415f - 3.1415f / 4);
 
         bullet.setShapeX(shapex);
         bullet.setShapeY(shapey);
 
     }
 
+    @Override
+    public Entity createBullet(Entity shooter, GameData gameData) {
+
+        PositionPart positionPart = shooter.getPart(PositionPart.class);
+
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+
+        float radians = positionPart.getRadians();
+        float dt = gameData.getDelta();
+        float speed = 350;
+
+        Entity bullet = new Bullet();
+        bullet.setRadius(2);
+
+        float bx = MathUtils.cos(radians) * shooter.getRadius() * bullet.getRadius();
+        float by = MathUtils.sin(radians) * shooter.getRadius() * bullet.getRadius();
+
+        bullet.add(new PositionPart(bx + x, by + y, radians));
+        bullet.add(new LifePart(1));
+        bullet.add(new MovingPart(0, 50000, speed, 5));
+        bullet.add(new TimerPart(1));
+
+        bullet.setShapeX(new float[2]);
+        bullet.setShapeY(new float[2]);
+
+        return bullet;
+    }
 }
--- MyAsteroidsGame/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletPlugin.java
+++ MyAsteroidsGame/Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletPlugin.java
@@ -33,48 +33,48 @@ public class BulletPlugin implements IGamePluginService {
      */
     @Override
     public void start(GameData gameData, World world) {
-        // Add entities to the world
-        bullet = createBullet(gameData);
-        world.addEntity(bullet);
+//        // Add entities to the world
+//        bullet = createBullet(gameData);
+//        world.addEntity(bullet);
     }
 
-    /**
-     * Creates a new bullet entity based on the shooter entity's position.
-     *
-     * @param gameData The game data object.
-     * @return The new bullet entity.
-     */
-    public Entity createBullet(GameData gameData) {
-        PositionPart shooterPositionPart = this.shooter.getPart(PositionPart.class);
-        MovingPart shooterMovingPart = this.shooter.getPart(MovingPart.class);
-
-        float deacceleration = 0;
-        float acceleration = 0;
-        float maxSpeed = 550;
-        float rotationSpeed = 0;
-        float radians = shooterPositionPart.getRadians();
-
-        Entity bullet = new Bullet();
-
-        bullet.setRadius(1);
-
-        float bx = MathUtils.cos(radians) * this.shooter.getRadius() * bullet.getRadius();
-        float by = MathUtils.sin(radians) * this.shooter.getRadius() * bullet.getRadius();
-
-        float x = bx + shooterPositionPart.getX();
-        float y = by + shooterPositionPart.getY();
-
-        float shootSpeed = maxSpeed + shooterMovingPart.getSpeed();
-
-        bullet.setShapeX(new float[4]);
-        bullet.setShapeY(new float[4]);
-        bullet.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed, shootSpeed));
-        bullet.add(new PositionPart(x, y, radians));
-        bullet.add(new LifePart(1, 1));
-
-
-        return bullet;
-    }
+//    /**
+//     * Creates a new bullet entity based on the shooter entity's position.
+//     *
+//     * @param gameData The game data object.
+//     * @return The new bullet entity.
+//     */
+//    public Entity createBullet(GameData gameData) {
+//        PositionPart shooterPositionPart = this.shooter.getPart(PositionPart.class);
+//        MovingPart shooterMovingPart = this.shooter.getPart(MovingPart.class);
+//
+//        float deacceleration = 0;
+//        float acceleration = 0;
+//        float maxSpeed = 550;
+//        float rotationSpeed = 0;
+//        float radians = shooterPositionPart.getRadians();
+//
+//        Entity bullet = new Bullet();
+//
+//        bullet.setRadius(1);
+//
+//        float bx = MathUtils.cos(radians) * this.shooter.getRadius() * bullet.getRadius();
+//        float by = MathUtils.sin(radians) * this.shooter.getRadius() * bullet.getRadius();
+//
+//        float x = bx + shooterPositionPart.getX();
+//        float y = by + shooterPositionPart.getY();
+//
+//        float shootSpeed = maxSpeed + shooterMovingPart.getSpeed();
+//
+//        bullet.setShapeX(new float[4]);
+//        bullet.setShapeY(new float[4]);
+//        bullet.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed, shootSpeed));
+//        bullet.add(new PositionPart(x, y, radians));
+//        bullet.add(new LifePart(1, 1));
+//
+//
+//        return bullet;
+//    }
 
     /**
      * Stops the plugin by removing the bullet entity from the game world.
@@ -84,7 +84,12 @@ public class BulletPlugin implements IGamePluginService {
      */
     @Override
     public void stop(GameData gameData, World world) {
-        world.removeEntity(this.bullet);
+        for (Entity e : world.getEntities()) {
+            if (e.getClass() == Bullet.class) {
+                world.removeEntity(e);
+            }
+        }
+//        world.removeEntity(this.bullet);
 
     }
 }
