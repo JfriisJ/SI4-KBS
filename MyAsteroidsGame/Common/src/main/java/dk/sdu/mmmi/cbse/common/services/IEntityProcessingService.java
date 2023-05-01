f3c22437afd04dc3afdd41e8c6c702823800653f
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-05-01 18:58:21 +0200

    added missing javadoc

--- MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -6,7 +6,6 @@ import dk.sdu.mmmi.cbse.common.data.World;
 public interface IEntityProcessingService {
 	/**
 	 * This interface is responsible for processing entities in the game.
-	 *
 	 * - Pre-conditions: The entity exists.
 	 * - Post-conditions: The entity has been processed.
 	 *
