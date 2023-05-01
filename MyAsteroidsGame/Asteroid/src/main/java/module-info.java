d243128a561e5866e411c51ea8dae3f0cdcd04e5
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-05-01 14:06:28 +0200

    added module-info.java and removed META-INF.

---
+++ MyAsteroidsGame/Asteroid/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IAsteroidSplitter;
+
+module Asteroid {
+	requires Common;
+
+	provides IGamePluginService with dk.sdu.mmmi.cbse.asteroidsystem.AsteroidPlugin;
+	provides IEntityProcessingService with dk.sdu.mmmi.cbse.asteroidsystem.AsteroidProcessingService;
+	provides IAsteroidSplitter with dk.sdu.mmmi.cbse.asteroidsystem.AsteroidSplitterImpl;
+
+	exports dk.sdu.mmmi.cbse.asteroidsystem;
+}
