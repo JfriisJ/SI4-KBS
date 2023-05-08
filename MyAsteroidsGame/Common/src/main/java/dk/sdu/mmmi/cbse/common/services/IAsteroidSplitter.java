75654e848b3a54fa185b5c2e8b2b6e3860b21202
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-05-08 09:32:40 +0200

    was missing pre and post conditions in javadoc

--- MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IAsteroidSplitter.java
+++ MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IAsteroidSplitter.java
@@ -10,6 +10,9 @@ import dk.sdu.mmmi.cbse.common.data.World;
 public interface IAsteroidSplitter {
 	/**
 	 * Creates new asteroids when an asteroid is destroyed.
+	 * - Preconditions: The entity exists and has been processed by ProcessingService.
+	 * - Postconditions: New asteroids have been created.
+	 *
 	 * @param e The entity representing the destroyed asteroid.
 	 * @param w The world in which the new asteroids will be created.
 	 */
--- MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IBulletCreateService.java
+++ MyAsteroidsGame/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IBulletCreateService.java
@@ -6,6 +6,8 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 public interface IBulletCreateService {
 	/**
 	 * Interface for a service that creates bullets.
+	 * - Preconditions: The entity exists and has been processed by ProcessingService.
+	 * - Postconditions: A bullet has been created and attached to the entity.
 	 *
 	 * @param entity The entity that the bullet will be attached to.
 	 * @param gameData The game data containing information on the game state.
