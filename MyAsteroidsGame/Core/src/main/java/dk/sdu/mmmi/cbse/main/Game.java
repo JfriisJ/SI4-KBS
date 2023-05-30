cbd058b8e13306c72cd1e1af64ed4063e0ebfc4b
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-05-30 09:42:14 +0200

    rearranged create method

--- MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
+++ MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -9,7 +9,6 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.util.SPILocator;
 import dk.sdu.mmmi.cbse.components.IGameEntityPluginServiceInjection;
