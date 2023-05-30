646749addcc06da564a0e0bb026b2e22170e5bd8
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-05-30 09:06:32 +0200

    corrected som basic setup in main

--- MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ MyAsteroidsGame/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -2,19 +2,19 @@ package dk.sdu.mmmi.cbse.main;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		int width = 1600;
-		int height = 720;
-
 		Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-		cfg.setTitle("Asteroids");
-		cfg.setWindowedMode(width, height);
-		cfg.setWindowSizeLimits(width, height, width, height);
+		cfg.setWindowedMode(1920/2, 1080/2);
 		cfg.setResizable(false);
+
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.scan("dk.sdu.mmmi.cbse.main");
+		applicationContext.refresh();
 
 		new Lwjgl3Application(new Game(), cfg);
 
